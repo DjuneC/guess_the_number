7617a1f856879b7519dba149b3d03650ba6da0a7
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-15 23:08:43 -0800

    Add logic for guessing part 1

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import random
+
 level_dict = {
     'easy': 10,
     'hard': 5
@@ -9,19 +11,21 @@ def check_attempt(hidden_number, user_guess):
 
     if user_guess == hidden_number:
         win = True
+        return {"win": win}
 
-    guess = "low" if user_guess > hidden_number else "high"
+    guess = "low" if user_guess < hidden_number else "high"
 
     return {"win": win, "guess": guess}
 
 
 def main():
-    magic_number = 100
+    magic_number = random.randint(1, 100)
+    print(magic_number)
     attempts = 0
     win = False
 
     print("Welcome to the Number Guessing Game!")
-    print(f"I'm thinking of a number between 1 and {magic_number}.")
+    print(f"I'm thinking of a number between 1 and 100.")
 
 
     while True:
@@ -39,20 +43,21 @@ def main():
 
         print(f"You have {attempts} attempts remaining to guess the number")
 
-        user_attempt = input("Make a guess:\n--> ")
+        user_attempt = int(input("Make a guess:\n--> "))
 
         decision = check_attempt(magic_number, user_attempt)
 
         if decision["win"]:
             break
 
-        print(f"Too {decision["guess"]}")
+        print(f"Too {decision["guess"]}.\n")
 
+        attempts -= 1
 
-
-
-
-
+    if attempts == 0:
+        print(f"It was so simple and yet you could not find it, hidden number was: {magic_number}")
+    else:
+        print("Hoorah! You guessed it right.")
 
 
 if __name__ == "__main__":
